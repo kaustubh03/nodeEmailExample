904935ad82d094dc0881b7af08e76070a4218a2a
Kaustubh Saxena <saxena.kaustubh03@gmail.com>
2019-07-27 22:08:55 +0530

    initial commit recursive curse

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const nodemailer = require("nodemailer");
+const cors = require("cors");
+
+
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+const port = 5000;
+
+
+
+
+app.post("/mail", (req, res) => {
+    let body = req.body;
+    var transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: body.sender_email,
+        pass: body.sender_password
+      }
+    });
+
+    var mailOptions = {
+        from: body.sender_email,
+        to: body.reciever_email,
+        subject: body.subject,
+        text: body.body
+    };
+    transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+            console.log(error);
+            res.send("Error")
+        } else {
+            console.log('Email sent: ' + info.response);
+            res.send("Email sent: " + info.response);
+        }
+    });
+});
+
+app.listen(port, () => console.log(`Email Service Running on ${port}`));
